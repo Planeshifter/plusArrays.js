c9a6a8c3bb3b5c7e78630783d9c6f580854faf90
Philipp Burckhardt <pburckhardt@outlook.com>
2015-01-15 00:46:58 -0500

    Update array.js

--- src/array.js
+++ src/array.js
@@ -17,11 +17,11 @@ function registerMethod(obj, name, fun){
 registerMethod(Array.prototype, "compare",function(testArr){
   if (this.length != testArr.length) return false;
   for (var i = 0; i < testArr.length; i++) {
-  if (this[i].compare) {
-      if (!this[i].compare(testArr[i])) return false;
-  }
-  if (this[i] !== testArr[i]) return false;
-  }
+	  if (this[i].compare) {
+	      if (!this[i].compare(testArr[i])) return false;
+	  }
+	  if (this[i] !== testArr[i]) return false;
+	}
   return true;
 });
 
