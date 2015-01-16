52c642e1e2eb0a4787587bb9a11495946bc96495
Planeshifter <pburckhardt@outlook.com>
2015-01-16 12:25:33 -0500

    new methods

--- test/test.js
+++ test/test.js
@@ -1,1 +1,12 @@
+require("../lib/array.js");
 var chai = require("chai");
+var assert = chai.assert;
+
+describe('Array.protoype', function () {
+  describe('.sum', function () {
+    var a = [1,2,3];
+    it('sums the elements of the array', function(){
+      assert.equal(a.sum(), 6);
+    });
+  });
+});
