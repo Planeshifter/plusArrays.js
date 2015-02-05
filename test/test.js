b06983116523efc4126e6850e1e4906475907bfb
Planeshifter <pburckhardt@outlook.com>
2015-02-05 14:12:53 -0500

    cumulative methods added

--- test/test.js
+++ test/test.js
@@ -9,4 +9,28 @@ describe('Array.protoype', function () {
       assert.equal(a.sum(), 6);
     });
   });
+  describe('.cumsum', function () {
+    var a = [1,2,3];
+    it('returns a vector whose elements are cumulative sums of input elements', function(){
+      assert.deepEqual(a.cumsum(), [1,3,6]);
+    });
+  });
+  describe('.cumprod', function () {
+    var a = [1,2,3,4];
+    it('returns a vector whose elements are cumulative product of input elements', function(){
+      assert.deepEqual(a.cumprod(), [1,2,6,24]);
+    });
+  });
+  describe('.cummin', function () {
+    var a = [1,2,3,2];
+    it('returns a vector whose elements are cumulative minima of input elements', function(){
+      assert.deepEqual(a.cummin(), [1,1,1,1]);
+    });
+  });
+  describe('.cummax', function () {
+    var a = [1,2,3,2];
+    it('returns a vector whose elements are cumulative maxima of input elements', function(){
+      assert.deepEqual(a.cummax(), [1,2,3,3]);
+    });
+  });
 });
